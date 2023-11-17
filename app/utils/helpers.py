d56161896a4980b3c7a280d9226278c5da3a52c9
Niko <niko.summanen1@gmail.com>
2023-11-17 23:52:08 +0200

    Refactor helpers

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,75 +1,46 @@
 from sqlalchemy import text
 from datetime import datetime
-from sqlalchemy import text
 from ..models.area import Area
 import bcrypt
 from zxcvbn import zxcvbn
 from ..utils.db import Database
 
 def get_areas():
-    db = Database()
-
-    sql = text("""SELECT id, topic FROM areas""")
     areas:list[Area] = []
 
-
-    for result in db.fetch_all(sql):
+    sql = text("""SELECT id, topic FROM areas""")
+    for result in Database().fetch_all(sql):
         area = Area(result["topic"], result["id"])
         areas.append(area)
 
     return areas
 
 def username_exists(username):
-    db = Database()
-
     sql = text("""SELECT COUNT(*) FROM users u WHERE u.username = :username""")
-    if db.fetch_one(sql, {"username" : username})["count"] == 0:
+    if Database().fetch_one(sql, {"username" : username})["count"] == 0:
         return False
 
     return True
 
 def verify_login(request):
-    db = Database()
-
     username = request.form["username"]
-    sql = text("""SELECT u.id, u.password FROM users u WHERE u.username = :username""")
-    result = db.fetch_one(sql, {"username" : username})
 
-    if result == None:
+    sql = text("""SELECT u.id, u.password FROM users u WHERE u.username = :username""")
+    result = Database().fetch_one(sql, {"username" : username})
+
+    if not result:
         return None
 
-    id = result["id"]
-    password_hash = result["password"].tobytes()
-
-    if check_password(password_hash, request.form["password"]):
-        return id
+    if bcrypt.checkpw(request.form["password"].encode('utf-8'), result["password"].tobytes()):
+        return result["id"]
 
     return None
 
 def hash_password(password):
-    password_bytes = password.encode('utf-8')
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password_bytes, salt)
-
-    return hashed_password
-
-def check_password(hashed_password, user_password):
-    password_bytes = user_password.encode('utf-8')
-
-    if bcrypt.checkpw(password_bytes, hashed_password):
-        return True
-    else:
-        return False
-
-# TODO: Limit execution time to prevent DoS    
 def is_password_secure(password):
-    strength = zxcvbn(password)
-
-    if strength["score"] >= 4:
-        return True
-    
-    return False
+    return zxcvbn(password)["score"] >= 4
 
 def time_ago(date):
     now = datetime.now()
