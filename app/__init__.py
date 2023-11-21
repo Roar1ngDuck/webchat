9cd64d841d76024d4c1a9367563e76ab38ad182b
Niko <niko.summanen1@gmail.com>
2023-11-21 23:59:34 +0200

    Add secure cookie attributes

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+from .chat.views import chat_blueprint
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = getenv("SECRET_KEY")
+    
+    if getenv("ENV") == "PROD":
+        app.config.update(
+            SESSION_COOKIE_SECURE = True,
+            SESSION_COOKIE_HTTPONLY = True,
+            SESSION_COOKIE_SAMESITE = "Strict",
+        )
 
-    from .chat.views import chat_blueprint
     app.register_blueprint(chat_blueprint)
 
     return app
