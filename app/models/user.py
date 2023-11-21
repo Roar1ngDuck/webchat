a7fb60e587a0a4cdca6168fa8d103901a55f0a74
Niko <niko.summanen1@gmail.com>
2023-11-22 00:30:54 +0200

    Add admin users

--- app/models/user.py
+++ app/models/user.py
@@ -3,11 +3,12 @@ from ..utils import helpers
 from ..utils.db import Database
 
 class User:
-    def __init__(self, username, password):
+    def __init__(self, username, password, is_admin = False):
         self.db = Database()
         self.username = username
         self.password = helpers.hash_password(password)
+        self.is_admin = is_admin
 
     def insert(self):
-        sql = text("""INSERT INTO users (username, password) VALUES (:username, :password) RETURNING id""")
-        self.id = self.db.insert_one(sql, {"username" : self.username, "password" : self.password})["id"]
+        sql = text("""INSERT INTO users (username, password, is_admin) VALUES (:username, :password, :is_admin) RETURNING id""")
+        self.id = self.db.insert_one(sql, {"username" : self.username, "password" : self.password, "is_admin" : self.is_admin})["id"]
--- app/utils/db.py
+++ app/utils/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 from os import getenv
 from threading import Lock
+from ..utils import helpers
 
 class Database:
     _instance = None
@@ -14,11 +15,12 @@ class Database:
                 if getenv("ENV") == "TEST":
                     cls._instance._drop_tables()  # Drop tables if in test environment
                 cls._instance._initialize_tables()
+                cls._instance._create_admin_user()
         return cls._instance
     
     def _drop_tables(self):
         drop_tables_sql = """
-        DROP TABLE IF EXISTS messages, threads, areas, users CASCADE;
+        DROP TABLE IF EXISTS messages, threads, areas, users, secret_area_privileges CASCADE;
         """
         with self.engine.connect() as connection:
             with connection.begin():
@@ -26,31 +28,45 @@ class Database:
 
     def _initialize_tables(self):
         table_creation_sql = """
-        CREATE TABLE IF NOT EXISTS messages (
+        CREATE TABLE IF NOT EXISTS areas (
             id SERIAL PRIMARY KEY,
-            thread integer NOT NULL,
-            sender integer NOT NULL,
-            text TEXT NOT NULL,
-            sent_time TIMESTAMP WITHOUT TIME ZONE DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC')
+            topic TEXT NOT NULL,
+            is_secret boolean NOT NULL
         );
         CREATE TABLE IF NOT EXISTS threads (
             id SERIAL PRIMARY KEY,
-            area integer NOT NULL,
+            area integer NOT NULL REFERENCES areas(id),
             title TEXT NOT NULL
-        );
-        CREATE TABLE IF NOT EXISTS areas (
-            id SERIAL PRIMARY KEY,
-            topic TEXT NOT NULL
         );
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
-            username TEXT NOT NULL,
-            password bytea NOT NULL
+            username TEXT NOT NULL UNIQUE,
+            password bytea NOT NULL,
+            is_admin boolean NOT NULL
+        );
+        CREATE TABLE IF NOT EXISTS messages (
+            id SERIAL PRIMARY KEY,
+            thread integer NOT NULL REFERENCES threads(id),
+            sender integer NOT NULL REFERENCES users(id),
+            text TEXT NOT NULL,
+            sent_time TIMESTAMP WITHOUT TIME ZONE DEFAULT (CURRENT_TIMESTAMP AT TIME ZONE 'UTC')
+        );
+        CREATE TABLE IF NOT EXISTS secret_area_privileges (
+            id SERIAL PRIMARY KEY,
+            area_id integer NOT NULL REFERENCES areas(id),
+            user_id integer NOT NULL REFERENCES users(id)
         );
         """
         with self.engine.connect() as connection:
             with connection.begin():
                 connection.execute(text(table_creation_sql))
+
+    def _create_admin_user(self):
+        admin_creation_sql = text("""INSERT INTO users (username, password, is_admin) VALUES (:username, :password, :is_admin) ON CONFLICT (username) DO NOTHING""")
+        with self.engine.connect() as connection:
+            with connection.begin():
+                connection.execute(admin_creation_sql, {"username" : "admin", "password" : helpers.hash_password(getenv("ADMIN_PASSWORD")), "is_admin" : True})
+
 
     def fetch_all(self, sql, params=None):
         with self.engine.connect() as connection:
