b27fc48fb4c21d3cba0b1483fde29d0facbeaf9b
Niko <niko.summanen1@gmail.com>
2023-12-17 21:35:26 +0200

    Add more tests

--- tests/integration/test_chat_views.py
+++ tests/integration/test_chat_views.py
@@ -2,6 +2,7 @@ import pytest
 from app import create_app
 from dotenv import load_dotenv
 
+
 @pytest.fixture(scope='module')
 def test_client():
     load_dotenv(dotenv_path='.env.test')
@@ -18,14 +19,17 @@ def test_client():
 
     ctx.pop()
 
+
 def login(test_client, username, password):
     return test_client.post('/login', data=dict(
         username=username,
         password=password
     ), follow_redirects=True)
 
+
 def logout(test_client):
     return test_client.get('/logout', follow_redirects=True)
+
 
 def test_register(test_client):
     new_user_data = {
@@ -42,6 +46,7 @@ def test_register(test_client):
     assert login_response.status_code == 200
     assert b"Logged in as" in login_response.data  # Check for a logged in status
 
+
 def test_register_weak_password(test_client):
     new_user_data = {
         "username": "testuser2",
@@ -52,6 +57,7 @@ def test_register_weak_password(test_client):
     response = test_client.post('/register', data=new_user_data, follow_redirects=True)
     assert response.status_code == 200
     assert b"Password too weak" in response.data
+
 
 def test_register_unmatching_password(test_client):
     new_user_data = {
@@ -65,38 +71,82 @@ def test_register_unmatching_password(test_client):
     assert b"Passwords don" in response.data
 
 
-def test_create_area_in_index_route(test_client):
-    login(test_client, 'testuser1', 'VBt8fETYzn$64ecARjmG')
-
+def test_create_area(test_client):
     new_area_data = {"topic": "Test Area 123"}
     response = test_client.post('/create_area', data=new_area_data, follow_redirects=True)
 
     assert response.status_code == 200
     assert b"Test Area 123" in response.data  # Check if the new area is listed
 
-    logout(test_client)
 
-def test_create_thread_in_view_area_route(test_client):
-    login(test_client, 'testuser1', 'VBt8fETYzn$64ecARjmG')
+def test_create_invalid_area(test_client):
+    new_area_data = {"topic": "A" * 100}
+    response = test_client.post('/create_area', data=new_area_data, follow_redirects=True)
 
-    area_id = 1
-    new_thread_data = {"title": "Test Thread 123", "message" : "Thread initial message"}
-    response = test_client.post(f'/area/{area_id}/create_thread', data=new_thread_data, follow_redirects=True)
+    assert b"Invalid area topic" in response.data  # Check if the new area is listed
+
+
+def test_view_nonexistent_area(test_client):
+    response = test_client.get('/area/100', follow_redirects=True)
+
+    assert b"Area does not exist" in response.data
+
+
+def test_create_thread(test_client):
+    new_thread_data = {"title": "Test Thread 123", "message": "Thread initial message"}
+    response = test_client.post('/area/1/create_thread', data=new_thread_data, follow_redirects=True)
 
     assert response.status_code == 200
     assert b"Test Thread 123" in response.data  # Check if the new thread is listed
-    assert b">1</span>" in response.data # Check if initial message is listed
+    assert b">1</span>" in response.data  # Check if initial message is listed
 
-    logout(test_client)
 
-def test_create_message_in_view_thread_route(test_client):
-    login(test_client, 'testuser1', 'VBt8fETYzn$64ecARjmG')
+def test_create_invalid_thread(test_client):
+    new_thread_data = {"title": "A" * 100, "message": "Thread initial message"}
+    response = test_client.post('/area/1/create_thread', data=new_thread_data, follow_redirects=True)
 
-    thread_id = 1
+    assert b"Invalid thread title" in response.data
+
+
+def test_view_nonexistent_thread(test_client):
+    response = test_client.get('/thread/100', follow_redirects=True)
+
+    assert b"Thread does not exist" in response.data
+
+
+def test_send_message(test_client):
     new_message_data = {"message": "New message in thread"}
-    response = test_client.post(f'/thread/{thread_id}/send_message', data=new_message_data, follow_redirects=True)
+    response = test_client.post('/thread/1/send_message', data=new_message_data, follow_redirects=True)
 
     assert response.status_code == 200
     assert b"New message in thread" in response.data  # Check if the new message is listed in the thread
 
-    logout(test_client)
+
+def test_send_invalid_message(test_client):
+    new_message_data = {"message": "A" * 2000}
+    response = test_client.post('/thread/1/send_message', data=new_message_data, follow_redirects=True)
+
+    assert b"Invalid message" in response.data
+
+
+def test_search(test_client):
+    response = test_client.get('/search?query=t', follow_redirects=True)
+
+    assert b"Test Area 123" in response.data
+    assert b"Test Thread 123" in response.data
+    assert b"Thread initial message" in response.data
+    assert b"New message in thread" in response.data
+
+
+def test_delete_message(test_client):
+    response = test_client.post('/delete_message/1/1', follow_redirects=True)
+
+    assert response.status_code == 200
+    assert b"Thread initial message" not in response.data
+
+
+def test_delete_thread(test_client):
+    response = test_client.post('/delete_thread/1', data={}, follow_redirects=True)
+
+    assert response.status_code == 200
+    assert b"Test Thread 123" not in response.data
