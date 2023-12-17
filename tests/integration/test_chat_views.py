f85c3123d877771e7d0d370df5a5984ea31951e7
Niko <niko.summanen1@gmail.com>
2023-12-17 16:50:40 +0200

    Update tests

--- tests/integration/test_chat_views.py
+++ tests/integration/test_chat_views.py
@@ -51,7 +51,7 @@ def test_register_weak_password(test_client):
 
     response = test_client.post('/register', data=new_user_data, follow_redirects=True)
     assert response.status_code == 200
-    assert b"Password is too weak" in response.data
+    assert b"Password too weak" in response.data
 
 def test_register_unmatching_password(test_client):
     new_user_data = {
@@ -69,7 +69,7 @@ def test_create_area_in_index_route(test_client):
     login(test_client, 'testuser1', 'VBt8fETYzn$64ecARjmG')
 
     new_area_data = {"topic": "Test Area 123"}
-    response = test_client.post('/', data=new_area_data, follow_redirects=True)
+    response = test_client.post('/create_area', data=new_area_data, follow_redirects=True)
 
     assert response.status_code == 200
     assert b"Test Area 123" in response.data  # Check if the new area is listed
@@ -81,7 +81,7 @@ def test_create_thread_in_view_area_route(test_client):
 
     area_id = 1
     new_thread_data = {"title": "Test Thread 123", "message" : "Thread initial message"}
-    response = test_client.post(f'/area/{area_id}', data=new_thread_data, follow_redirects=True)
+    response = test_client.post(f'/area/{area_id}/create_thread', data=new_thread_data, follow_redirects=True)
 
     assert response.status_code == 200
     assert b"Test Thread 123" in response.data  # Check if the new thread is listed
@@ -94,7 +94,7 @@ def test_create_message_in_view_thread_route(test_client):
 
     thread_id = 1
     new_message_data = {"message": "New message in thread"}
-    response = test_client.post(f'/thread/{thread_id}', data=new_message_data, follow_redirects=True)
+    response = test_client.post(f'/thread/{thread_id}/send_message', data=new_message_data, follow_redirects=True)
 
     assert response.status_code == 200
     assert b"New message in thread" in response.data  # Check if the new message is listed in the thread
